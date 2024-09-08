291a4f61e245630f2ba444941b48ee0a5377bc47
yucaib <y.canfee@gmail.com>
2024-09-09 02:39:17 +0300

    some parenthesis

--- src/triangle.java
+++ src/triangle.java
@@ -2,17 +2,16 @@ package src;
 import java.util.Scanner;
 public class triangle {
     public static void main(String[] args) {
-        double a, b, c, angle ;
+        int a, b, angle ;
+        double c;
         Scanner sc = new Scanner(System.in);
         System.out.print("A kenarının uzunluğunu giriniz");
-        a = sc.nextDouble();
+        a = sc.nextInt();
         System.out.print("B kenarının uzunluğunu giriniz");
-        b = sc.nextDouble();
+        b = sc.nextInt();
         System.out.print("A ve B kenarları arasında ki açıyı giriniz");
-        angle = sc.nextDouble();
-
-        c = Math.sqrt((a*a) + (b*b) - 2*a*b*(Math.cos(angle)));
-
+        angle = sc.nextInt();
+        c = Math.sqrt((a*a) + (b*b) - (2*(a*b))*(Math.cos(angle)));
         System.out.println("Hedeflenen kenarın uzunluğu: "+ c  );
     }
 }
